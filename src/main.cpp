c602faf97865cb1374195cdedfe8f55c33db20bd
WANGSHAOXIA11 <115467566+WANGSHAOXIA11@users.noreply.github.com>
2023-07-05 22:56:54 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,56 @@
 #include "queue.h"
 #include"sort.h"
 #include"AVLBST.h"
+#include"graph.h"
+#include"blackred.h"
+#include"kruskal.h"
+#include"hash.h"
 using namespace std;
-
+void testkruskal();
 void testBST();
 void testOfSeqlist();
 void testOfLinkedList();
 void testOfTree();
 void testOfQueue();
-void test_sort(int i);
-
+void test_sort();
+void testRedBlackTree();
+void test_hash();
+void testGraph();
 int main()
 {
-    cout << "1:queue 2:BST";
-    int a;
-    cin >> a;
-    if (a == 1)
-        testOfQueue();
-    if (a == 2)
-        testBST();
+    while (1)
+    {
+        cout << "1:测试线性表和链表\n2:测试Queue\n3:测试树形结构朋辈导生关系\n4:测试二分查找树\n5:测试hash\n6:测试排序\n7测试友谊关系最短路\n8测试红黑树\n9测试最小生成树";
+        int a;
+        cin >> a;
+        if (a == 1)
+        {
+            testOfSeqlist();
+            testOfLinkedList();
+        }
+          
+        if (a == 2)
+            testOfQueue();
+        if (a == 3)
+            testOfTree();
+        if(a==4)
+            testBST();
+        if (a == 5)
+            test_hash();
+        if(a==6)
+            test_sort();
+        if(a==7)
+            testGraph();
+        if(a==8)
+          testRedBlackTree();
+        if (a == 9)
+          testkruskal();
+        if (a == 0)
+            break;
+
+    }
+    
+    
 
 }
 void testBST()
@@ -58,18 +90,10 @@ void testBST()
         if (op == 1)
             bst.inorderTraversal(); // 遍历二分查找树
         if (op == 2) {
-            int id;
-            string name;
-            int score;
-
-            cout << "请输入学生ID：";
-            cin >> id;
-            cout << "请输入学生姓名：";
-            cin >> name;
-
-            cout << "请输入学生成绩：";
-            cin >> score;
-
+            int id=5;
+            string name="Jack";
+            int score=93;
+            cout << "测试样例数据为5,Jack,93" << endl;
             // 创建新的 Student 对象
             AVLBSTree::Student student;
             student.id = id;
@@ -85,27 +109,26 @@ void testBST()
             }
             outputFile << student.id << "," << student.name << "," << student.score << endl;
             outputFile.close();
+            bst.inorderTraversal();
         }
 
         if (op == 3) {
-            int score;
-            cout << "请输入需要删除的学生分数" << endl;
-            cin >> score;
+            int score=93;
+            cout << "测试样例为删除成绩为93的学生" << endl;
             bst.remove(score);
+            bst.inorderTraversal();
         }
         if (op == 4) {
-            int prescore;
-            int postscore;
-            cout << "请输入修改前的分数" << endl;
-            cin >> prescore;
-            cout << "请输入修改后的分数" << endl;
-            cin >> postscore;
+            int prescore=100;
+            int postscore=99;
+            cout << "修改前的分数为100" << endl;
+            cout << "修改后的分数为99" << endl;
             bst.update(prescore, postscore);
+            bst.inorderTraversal();
         }
         if (op == 5) {
-            int score;
-            cout << "请输入要查找的分数" << endl;
-            cin >> score;
+            int score=90;
+            cout << "查找的分数为90" << endl;
             // 查询学生成绩为90的学生信息
             TreeNode* result = bst.search(score);
             if (result != nullptr) {
@@ -117,126 +140,419 @@ void testBST()
         }
     }
 }
-void testOfTree() {
-    string filename = "student.txt";
-    PeerTree peerTree = buildPeerTreeFromTxt(filename);
+void testOfTree() 
+{
+    PeerMentorshipTree tree;
 
-    // 示例查询
-    int studentId1 = 1;
-    int studentId2 = 2;
+    std::ifstream inputFile("treedata.txt");
+    std::string line;
+    cout << "学生ID\t" << "导师ID\t" << "年级\t" << "学生姓名" << endl;
+    while (std::getline(inputFile, line)) {
+        std::istringstream iss(line);
+        int studentID, mentorID, grade;
+        std::string studentName;
+        
 
-    if (peerTree.isPeerRelationship(studentId1, studentId2)) {
-        cout << "学生 " << studentId1 << " 和学生 " << studentId2 << " 为导生关系。" << endl;
+        if (iss >> studentID >> mentorID >> grade >> studentName) {
+            cout << studentID << "\t" << mentorID << "\t" << grade << "\t" << studentName << endl;
+            tree.insert(studentID, mentorID, grade, studentName);
+
+        }
+       
+    }
+    int mentorID=1, studentID=2;
+    cout << "导师的学号：1"<<endl;
+    cout << "学生的学号：2"<<endl;
+
+    if (tree.isDirectMentor(mentorID, studentID)) {
+        cout << "是直接导生关系" << endl;
+    }
+    else if (tree.isIndirectMentor(mentorID, studentID, 3)) {
+        cout << "是间接导生关系" << endl;
     }
     else {
-        cout << "学生 " << studentId1 << " 和学生 " << studentId2 << " 不是导生关系。" << endl;
+        cout << "不是导生关系" << endl;
+    }
+
+    mentorID = 1, studentID = 6;
+    cout << "导师的学号：1" << endl;
+    cout << "学生的学号：6" << endl;
+    if (tree.isDirectMentor(mentorID, studentID)) {
+        cout << "是直接导生关系" << endl;
+    }
+    else if (tree.isIndirectMentor(mentorID, studentID, 3)) {
+        cout << "是间接导生关系" << endl;
+    }
+    else {
+        cout << "不是导生关系" << endl;
+    }
+
+    mentorID = 3, studentID = 7;
+    cout << "导师的学号：3" << endl;
+    cout << "学生的学号：7" << endl;
+    if (tree.isDirectMentor(mentorID, studentID)) {
+        cout << "是直接导生关系" << endl;
+    }
+    else if (tree.isIndirectMentor(mentorID, studentID, 3)) {
+        cout << "是间接导生关系" << endl;
+    }
+    else {
+        cout << "不是导生关系" << endl;
     }
 }
 
 void testOfLinkedList() {
     LinkedList list;
+    list.display();
+    // Reading from file
+    list.readFromFile("listdata.txt");
+    while (1)
+    {
+        int choice;
+        list.display();
+        cout << "请输入操作" << endl;
+        cout << "1:增加学生\n2:查找学生\n3:修改学生\n4:删除学生\n0:退出" << endl;
+        cin >> choice;
+        if (choice == 1)
+        {
+            string name="Jack";int num=6;int grade=99;
+            cout << "测试数据为Jack,6,99" << endl;
+            list.insert(name, num, grade);
+            list.display();
+        }
+        else if (choice == 2)
+        {
+            int num=3;
+            cout << "查找学生的学号是3" << endl;
+            linkedlist::Student* student = list.search(num);
+            if (student) {
+                std::cout << "ID 为:" << num << "的学生" << endl;
+                std::cout << "Name: " << student->name << std::endl;
+                std::cout << "ID: " << student->id << std::endl;
+                std::cout << "Grade: " << student->grade << std::endl;
+            }
+            else {
+                std::cout << "没有找到\n";
+            }
+        }
+        else if (choice == 3)
+        {
+            cout << "修改学生的学号为3" << endl;
+            int num=3;
+            string name="Jason";int grade=88;
+            cout << "修改后的姓名是 Jason" << endl;
+            cout << "修改后的成绩是88" << endl;
+            list.update(num, name, grade);
+            list.display();
+        }
+        else if (choice == 4)
+        {
 
-    // Inserting students
-    list.insert("Alice", 1, 90);
-    list.insert("Bob", 2, 80);
-    list.insert("Charlie", 3, 85);
-
-    // Searching and displaying student
-    linkedlist::Student* student = list.search(2);
-    if (student) {
-        std::cout << "Found student with ID 2:\n";
-        std::cout << "Name: " << student->name << std::endl;
-        std::cout << "ID: " << student->id << std::endl;
-        std::cout << "Grade: " << student->grade << std::endl;
+            cout << "删除学生的学号为5" << endl;
+            int num=5;
+            list.remove(num);
+            list.display();
+        }
+        else if (choice == 0)
+        {
+            break;
+        }
+        else
+        {
+            cout << "操作无效" << endl;
+        }
     }
-    else {
-        std::cout << "Student with ID 2 not found.\n";
-    }
-
-    // Updating student
-    list.update(3, "Charlie Brown", 88);
-
-    // Removing student
-    list.remove(1);
-
     // Saving to file
-    list.wiriteToFile("students.txt");
+   // list.writeToFile("students.txt");
 }
 
 void testOfSeqlist() {
     SeqList list;
-    string filename = "student.txt";
+    string filename = "listdata.txt";
     // read from file
     list.readFromFile(filename);
     list.display();
 
-    // insert
-    seqlist::Student s1 = { "Dave", 4, 95 };
-    list.insert(s1);
+    while (1)
+    {
+        cout << "请输入你的操作\n1:增加学生数据\n2:删除学生数据\n3:修改学生数据\n4:查找学生数据\n0:退出";
+        int choice;
+        cin >> choice;
+        if (choice == 1)
+        {
+            cout << "测试数据为Dave 4 95" << endl;
+            seqlist::Student s1 = { "Dave", 4, 95 };
+            list.insert(s1);
+            list.display();
+        }
+        else if (choice == 2)
+        {
+            cout << "删除ID为2的学生" << endl;
+            list.deleteById(2);
+            list.display();
+        }
+        else if (choice == 3)
+        {
+            cout << "将ID为1的学生修改为 Alice 1 100"<<endl;
+            seqlist::Student s2 = { "Alice", 1, 100 };
+            list.updateById(1, s2);
+            list.display();
+        }
+        else if (choice == 4)
+        {
+            cout << "查找ID为3的数据";
+            seqlist::Student* s3 = list.searchById(3);
+            cout << s3->name << " " << s3->id << " " << s3->grade << endl;
+        }
+        else if (choice == 0)
+        {
+            break;
+        }
+        else
+        {
+            cout << "操作错误";
+        }
+    }
 
-    // delete
-    list.deleteById(2);
-
-    // update
-    seqlist::Student s2 = { "Alice", 1, 100 };
-    list.updateById(1, s2);
-
-    // search
-    seqlist::Student* s3 = list.searchById(3);
-    cout << s3->name << " " << s3->id << " " << s3->grade << endl;
-
-    // write to file
-    list.writeToFile(filename);
 }
 
 void testOfQueue() {
-    Queue q;
-    string filename = "student.txt";
-    q.readFromFile(filename);
+    unordered_map<string, pair<Queue, int>> courses;
+    courses["Math"] = { Queue(), 2 };
+    courses["English"] = { Queue(), 1 };
+    courses["History"] = { Queue(), 1 };
 
-    while (!q.isEmpty()) {
-        queue::Student student = q.dequeue();
-        std::cout << student.name << ' ' << student.id << '\n';
+    unordered_map<string, string> students;
+    students["Alice"] = "Math";
+    students["Bob"] = "English";
+    students["Charlie"] = "History";
+    students["Dave"] = "Math";
+    students["Eve"] = "English";
+
+    for (auto& student : students) {
+        string name = student.first;
+        string course = student.second;
+        queue::Student s{ name, 0 };
+        if (courses[course].first.size() < courses[course].second) {
+            courses[course].first.enqueue(s);
+            cout << name << " successfully enrolled in " << course << endl;
+        }
+        else {
+            cout << name << " failed to enroll in " << course << endl;
+        }
     }
 
 }
 
-
-void test_sort(int i)
+void test_sort()
 {
-    string filename = "student.txt";
-    //测试排序函数，0为插入排序，1为快速排序,2为堆排序
-    if (i == 0)
+    string filename = "sortdata.txt";
+    //
+    while (1)
     {
-        cout << "插入排序：" << endl;
-        SeqList list;
-        list.readFromFile(filename);
-        list.display();
-        cout << "*******************************************" << endl;
-        insert_sort(list);
-        list.display();
-        cout << "*******************************************" << endl;
+        int i;
+        cout << "测试排序函数，1为插入排序，2为快速排序,3为堆排序,0为退出" << endl;
+        cin >> i;
+        if (i == 1)
+        {
+            cout << "插入排序：" << endl;
+            SeqList list;
+            list.readFromFile(filename);
+            list.display();
+            cout << "*******************************************" << endl;
+            insert_sort(list);
+            list.display();
+            cout << "*******************************************" << endl;
+        }
+        else if (i == 2)
+        {
+            cout << "快速排序：" << endl;
+            SeqList list;
+            list.readFromFile(filename);
+            list.display();
+            cout << "*******************************************" << endl;
+            quick_sort(list, 0, list.get_length() - 1);
+            list.display();
+            cout << "*******************************************" << endl;
+        }
+        else if (i == 3)
+        {
+            cout << "堆排序：" << endl;
+            SeqList list;
+            list.readFromFile(filename);
+            list.display();
+            cout << "*******************************************" << endl;
+            heap_sort(list);
+            list.display();
+            cout << "*******************************************" << endl;
+        }
+        else if (i == 0)
+        {
+            break;
+        }
+        else
+        {
+            cout << "操作错误"<<endl;
+        }
     }
-    else if (i == 1)
+   
+}
+
+void testRedBlackTree()
+{
+    RedBlackTree tree;
+    tree.readStudentInfo("blackred.txt");
+
+    cout << "请选择您的操作：" << endl;
+    cout << "1. 统计男女生总数" << endl;
+    cout << "2. 修改学生信息" << endl;
+    cout << "3. 删除学生信息" << endl;
+    cout << "4. 查找学生信息" << endl;
+    cout << "5. 增加学生信息" << endl;
+    cout << "0. 退出 " << endl;
+    while (1)
     {
-        cout << "快速排序：" << endl;
-        SeqList list;
-        list.readFromFile(filename);
-        list.display();
-        cout << "*******************************************" << endl;
-        quick_sort(list, 0, list.get_length() - 1);
-        list.display();
-        cout << "*******************************************" << endl;
+        cout << "当前数据遍历结果如下" << endl;
+        tree.traverseTree(tree.getRoot());
+        int choice;
+        cin >> choice;
+        if (choice == 1)
+        {
+            int numMale, numFemale;
+            tree.countGenders(numMale, numFemale);
+            cout << "男生总数: " << numMale << ", 女生总数: " << numFemale << endl;
+        }
+        else if (choice == 2)
+        {
+            int studentID=10001;
+            string newName="Jack", newGender="male";
+            cout << "修改的学生ID: 1"<<endl;
+            cout << "新的姓名: Jack"<<endl;
+            cout << "新的性别: male"<<endl;
+            tree.modifyStudent(studentID, newName, newGender);
+            cout << "修改成功" << endl;
+        }
+        else if (choice == 3)
+        {
+            int studentID=10003;
+            cout << "要删除的学生ID: 10003";
+            tree.removeStudent(studentID);
+            cout << "删除成功" << endl;
+        }
+        else if (choice == 4)
+        {
+            int studentID=10005;
+            cout << "查找的学生ID: 10005";
+            Node* student = tree.searchStudent(studentID);
+            if (student != nullptr) {
+                cout << "ID: " << student->studentID << ", Name: " << student->name << ", Gender: " << student->gender << endl;
+            }
+            else {
+                cout << "未找到该学生信息" << endl;
+            }
+
+        }
+        else if (choice == 5)
+        {
+            int studentID=10003;
+            string name="Jane", gender="female";
+            cout << "增加的学生ID: 10003";
+            cout << "姓名: Jane";
+            cout << "请输入性别: female";
+            tree.insertStudent(studentID, name, gender);
+            cout << "添加成功" << endl;
+        }
+        else if (choice == 0)
+        {
+            break;
+        }
+        else
+        {
+            cout << "无效选择" << endl;
+
+        }
     }
-    else if (i == 2)
+}
+
+void testGraph() {
+    Graph g;
+    g.read_from_file("graphStudent.txt");
+    cout << "计算Alice 到 Eve 合作最好拉上谁" << endl;
+    std::string start = "Alice";
+    std::string end = "Eve";
+
+    std::vector<std::string> path = g.get_shortest_path(start, end);
+
+    std::cout << "从 " << start << " 到" << end << ": ";
+    for (const std::string& v : path) {
+        std::cout << v << ' ';
+    }
+    std::cout << endl;
+}
+
+void testkruskal()
+{
+    ifstream inputFile("graphstudent.txt");
+    if (!inputFile.is_open()) {
+        cout << "无法打开文件。" << endl;
+        return;
+    }
+
+    vector<Edge> edges;
+    vector<string> vertices;
+
+    string line;
+    while (getline(inputFile, line)) {
+        stringstream ss(line);
+        string source, destination;
+        int weight;
+
+        ss >> source >> destination >> weight;
+
+        edges.push_back({ source, destination, weight });
+        if (find(vertices.begin(), vertices.end(), source) == vertices.end())
+            vertices.push_back(source);
+        if (find(vertices.begin(), vertices.end(), destination) == vertices.end())
+            vertices.push_back(destination);
+    }
+
+    inputFile.close();
+
+    // 计算最小生成树
+    vector<Edge> minimumSpanningTree = kruskalMST(edges, vertices);
+
+    // 输出最小生成树的边及权重
+    cout << "Minimum Spanning Tree:" << endl;
+    for (const auto& edge : minimumSpanningTree) {
+        cout << edge.source << " -- " << edge.destination << " : " << edge.weight << endl;
+    }
+
+}
+void test_hash()
+{
+    SeqList students;
+    students.readFromFile("hash.txt");
+    Hash table = Hash(students.get_length() * 2 - 1);
+    for (int i = 0; i < students.get_length(); i++)
     {
-        cout << "堆排序：" << endl;
-        SeqList list;
-        list.readFromFile(filename);
-        list.display();
-        cout << "*******************************************" << endl;
-        heap_sort(list);
-        list.display();
-        cout << "*******************************************" << endl;
+        table.insert(students[i], i + 1);
     }
+    for (int i = 0; i < students.get_length(); i++)
+    {
+        hash_group info = table.search_seat(students[i]);
+        cout << info.stu.name << ' ' << info.seat_num << endl;
+    }
+    cout << "*************************************" << endl;
+    seqlist::Student s1;
+    s1.id = 21;
+    s1.grade = 99;
+    s1.name = "Mimosa";
+    table.insert(s1, students.get_length() + 1);
+    for (int i = 0; i < students.get_length(); i++)
+    {
+        hash_group info = table.search_seat(students[i]);
+        cout << info.stu.name << ' ' << info.seat_num << endl;
+    }
+    hash_group info = table.search_seat(s1);
+    cout << info.stu.name << ' ' << info.seat_num << endl;
 }
